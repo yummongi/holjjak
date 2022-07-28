2625a788c09b271b6c3246d595ddab1660fd608a
yummongi <yummongi@gmail.com>
2022-07-28 16:55:29 +0900

    홀짝 다른 버전 추가

--- src/Holjjak.java
+++ src/Holjjak.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
-public class Main {
+public class Holjjak {
     public static void main(String[] args) {
         int mylife = 10, yourlife = 10; //보유 구슬
         int myball, yourball = 0; //배팅 구슬
-        String answer = "";
+        String myanswer = "" , answer = "";
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("오징어 게임에 오신것을 환영합니다.");
@@ -14,30 +14,43 @@ public class Main {
         System.out.println("시작합니다.");
         System.out.println("배팅 하세요");
         System.out.println("=======================================");
+
         do {
             do {
-
                 System.out.print("자신이 베팅할 구슬 갯수를 입력하세요: (1~10 입력)");
                 myball = scanner.nextInt();
             } while (myball < 1 || myball > mylife);
             System.out.println("=======================================");
             System.out.println("시작합니다.");
 
-            yourball = (int) (Math.random() * yourlife) + 1; // 1 ~ 10 까지
+            yourball = (int) (Math.random() * 10) + 1; // 1 ~ 10 까지
 
-            System.out.println("상대방이 낸 구슬 갯수: " + yourball);
-            System.out.println("내가 낸 구슬 갯수: " + myball);
+            System.out.print("상대방의 구슬을 홀, 짝을 입력하여 추리하세요.");
+            myanswer = scanner.next();
+
+            if(yourball % 2 == 0){
+                answer = "짝";
+            }
+            else if (yourball % 2 != 0){
+                answer = "홀";
+            }
             System.out.println("=======================================");
-            if (myball > yourball) {
+            System.out.println("내가 낸 구슬 갯수 : " + myball);
+            System.out.println("상대가 낸 구슬 갯수 : " + yourball);
+            System.out.println("=======================================");
+
+            if(myanswer.equalsIgnoreCase(answer)){
                 mylife += yourball;
                 yourlife -= yourball;
                 System.out.println("승리하였습니다.");
-
-            } else if (myball == yourball) System.out.println("비겼습니다.");
-            else {
+            }
+            else if (!(myanswer.equalsIgnoreCase(answer))){
                 yourlife += myball;
                 mylife -= myball;
                 System.out.println("상대방한테 졌습니다.");
+            }
+            else{
+                System.out.println("비겼습니다.");
             }
             System.out.println("=======================================");
             System.out.println("현재 나의 구슬 갯수: " + mylife);
@@ -55,3 +68,6 @@ public class Main {
         }
     }
 }
+
+
+
